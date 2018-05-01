34cad7bf5daac83cb018b5400636f216135145b9
Laureen Perrin <laureencoupe@hotmail.fr>
2018-05-01 10:51:07 +0200

    adding of js functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 //__________________________________________________Déclaration des const utilisées dans le programme:
 const mainWrapperElt = document.querySelector("main_wrapper");
 
@@ -11,38 +11,8 @@ const mapZoneElt = document.querySelector("map");
 const footerElt = document.querySelector("footer");
 
 
-ajaxGet("https://js/lyon.json",function (reponse) {
-    var stationsTable = JSON.parse(reponse);
-  console.log(stationsTable);
-});
+//__________________________________________________Déclaration google map :
 
-//__________________________________________________création et ajout google map :
-function initMap() {
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: {lat: 45.750000, lng: 4.850000}
-        });
-
-        // Create an array of alphabetical characters used to label the markers.
-        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-        // Add some markers to the map.
-        // Note: The code uses the JavaScript Array.prototype.map() method to
-        // create an array of markers based on a given "locations" array.
-        // The map() method here has nothing to do with the Google Maps API.
-        var markers = locations.map(function(location, i) {
-          return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-          });
-        });
-
-        // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
-      var locations = ""
 
 
 //________________________________________________________________Déclarations des class :
@@ -76,23 +46,38 @@ class Station {
 
     
  function displayStations(stations) {
- var stationsArray = stations.map(station => new Station(station.name, station.address, station.position)); 
- for (let i = 0; i < stationsArray.length; i++) {
- const locStation = stationsArray[i].position;
- console.log(locStation);
-}
+  const stationsArray = stations.map(station => new Station(station.name, station.address, station.position)); 
+  for (let i = 0; i < stationsArray.length; i++) {
+    const locStation = stationsArray[i].displayPosition();
+    console.log(locStation);
+  }
+  return stationsArray;
 };
 
+function initMap(data) {
 
+        const googleMap = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {lat: 45.750000, lng: 4.850000}
+        });
+         createMarker(data);
+        return googleMap;
 
-
-
+};
+ 
+function createMarker () {
+    stationsArray.forEach(function (stations){
+                const marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(stations.position),
+                    map: googleMap
+                });
+              });
+  };
 
 //_________________________________________________appel AJAX:
-fetch('https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=b9eae55b32f61852fc6a740a3867d131bb01dd37')
-  // ligne 19 = function anonyme === function() {}
-  .then(reponse => reponse.json())
+fetch('https://cors-anywhere.herokuapp.com/https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=b9eae55b32f61852fc6a740a3867d131bb01dd37')
+  .then(infosStations => infosStations.json())
   .then(data => displayStations(data))
+  .then(data => initMap(data))
+  .then(data => createMarker(data))
   .catch(error => console.log(error));
-
-
